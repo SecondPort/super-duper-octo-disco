82caeca71b55fa0af069d787475686064530bea3
SecondPort <gomezmanulm@gmail.com>
2022-07-18 12:27:09 -0300

    mas binding

--- Primera-App/first-app/src/app/personas/personas.component.ts
+++ Primera-App/first-app/src/app/personas/personas.component.ts
@@ -11,5 +11,11 @@ import { Component } from "@angular/core";
 export class PersonasComponent{
     disable = false;
     mensaje = "No se ha pulsado el botón";
+    titulo = "";
     agregarPersona = () => this.mensaje = "Se ha agregado una persona";
+
+    modificarTitulo = (event:Event) => {
+        //debemos realizar un casteo para que el evento sea compatible con el tipo de dato que es
+        this.titulo = (<HTMLInputElement>event.target).value;
+    }
 }
