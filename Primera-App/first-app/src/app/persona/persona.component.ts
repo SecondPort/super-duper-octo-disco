7edca2b759bdb38769de04dbdc95ed703adc04f1
SecondPort <gomezmanulm@gmail.com>
2022-07-18 12:10:04 -0300

    event binding

--- Primera-App/first-app/src/app/persona/persona.component.ts
+++ Primera-App/first-app/src/app/persona/persona.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './persona.component.html',
   styleUrls: ['./persona.component.css']
 })
+/* The class PersonaComponent has a property called nombre, a property called apellido, a property
+called edad, and a method called getEdad. */
 export class PersonaComponent {
   nombre:string = 'Juan';
   apellido:string = 'Perez';
--- Primera-App/first-app/src/app/personas/personas.component.ts
+++ Primera-App/first-app/src/app/personas/personas.component.ts
@@ -1,10 +1,15 @@
+/* The PersonasComponent class is a component class that has a property called disable that is set to
+false */
 import { Component } from "@angular/core";
 
+/* A decorator that is used to define the metadata of the component. */
 @Component({
     selector: "app-personas",
     templateUrl: "./personas.component.html",
     styleUrls: ["./personas.component.css"]
 })
 export class PersonasComponent{
-
+    disable = false;
+    mensaje = "No se ha pulsado el botón";
+    agregarPersona = () => this.mensaje = "Se ha agregado una persona";
 }
